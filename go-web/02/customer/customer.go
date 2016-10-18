384d4b68b867df0340c98dfddb3925f4a2714dd0
Vikash Pandey <vikash.pandey@fisglobal.com>
2016-10-18 14:30:46 +0530

    api endpoints added

---
+++ go-web/02/customer/customer.go
@@ -0,0 +1,85 @@
+package customer
+
+import (
+    "fmt"
+    "encoding/json"
+)
+
+type Customer struct {
+    Name string `json:"name"`
+    Email string `json:"email"`
+    Phone string `json:"phone"`
+}
+
+type Customers []Customer
+
+var customers  = Customers {
+       { 
+        Name: "C1",
+        Email: "c1@in.com",
+        Phone: "9999999999",
+    },
+    {
+        Name: "C2",
+        Email: "c2@in.com",
+        Phone: "8899999988",
+    },
+}
+
+//GetCustomer based on supplied index
+func GetCustomer(index int) string {
+    var js = []byte("{}")
+    defer func() {
+        
+        if r := recover(); r != nil {
+            fmt.Printf("Panic Handled locally: %s\n", r)        
+            
+        }
+        
+    }()
+    //fmt.Printf("Returning Customer %s\n", customers[index -1 ])
+    custToReturn := customers[index - 1]
+    js, err := json.Marshal(custToReturn)
+    if err != nil {
+        fmt.Println(err)
+        return "Error marshalling customer"
+    }
+    fmt.Println("Returning from GetCustomer")
+    return string(js)
+}
+
+//GetCustomers gets all customers
+func GetCustomers() string {
+    fmt.Printf("Returning Customer %s\n", customers)
+    js, err := json.Marshal(customers)
+    if err != nil {
+        
+        return "Error marshalling customer"
+    }
+    return string(js)
+}
+
+//UpdateCustomer updates supplied customer, adds for now to the customers slice
+func UpdateCustomer(customer []byte) string {
+    var c Customer
+    
+    err := json.Unmarshal(customer, &c)
+    customers = append(customers, c)
+    if err != nil {
+        
+        return "Error unmarshalling customer"
+    }
+   js, err := json.Marshal(c)
+    if err != nil {
+        
+        return "Error marshalling customer"
+    }
+    return string(js)
+}
+
+//DeleteCustomer deletes customer at given index
+func DeleteCustomer(i int) string {
+    customers = append(customers[:i], customers[i+1:]...)
+    
+    return `{message: "Customer deleted"}`
+}
--- go-web/02/main.go
+++ go-web/02/main.go
@@ -6,6 +6,10 @@ import (
 	"github.com/vikash1976/goExperiments/go-web/02/page"
 	"net/http"
 	"regexp"
+	"github.com/vikash1976/goExperiments/go-web/02/customer"
+	"io"
+	"io/ioutil"
+	"strconv"
 	)
 
 var validPath = regexp.MustCompile("([a-zA-Z0-9]+)$")
@@ -57,13 +61,68 @@ func saveHandler(w http.ResponseWriter, r *http.Request, params httprouter.Param
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+func customersHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	setHeaders(&w)
+
+	io.WriteString(w, customer.GetCustomers())
+	
+}
+func customerHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	
+	custID, _ := strconv.Atoi(params.ByName("id"))
+	cust := customer.GetCustomer(custID)
+	
+	if len(cust) == 0 {
+		http.Error(w, "Invalid Index", http.StatusInternalServerError)
+		return
+	}
+	setHeaders(&w)
+	io.WriteString(w, cust)
+	
+}
+
+func customerUpdateHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	//fmt.Fprintln(w, "post update")
+	fmt.Println("Param is: ", params)
+	body, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        fmt.Println("some error")
+    }
+    fmt.Println(string(body))
+	 //var t map[string]interface{}
+	cust := customer.UpdateCustomer(body)
+	setHeaders(&w)
+    io.WriteString(w, cust)
+	
+}
+func customerDeleteHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	custID, _ := strconv.Atoi(params.ByName("id"))
+	cust := customer.DeleteCustomer(custID)
+	setHeaders(&w)
+    io.WriteString(w, cust)
+	
+}
+
+func setHeaders(w *http.ResponseWriter) {
+	(*w).Header().Set("Content-Type", "application/json; charset=utf-8")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+
+}
+
 func main() {
 	router := httprouter.New()
 	router.GET("/view/:file", makeHandler(viewHandler))
 	router.GET("/edit/:file", makeHandler(editHandler))
 	router.POST("/save/:file", makeHandler(saveHandler))
+	router.GET("/api/customer", customersHandler)
+	router.GET("/api/customer/:id", customerHandler)
+	router.PUT("/api/customer/:id", customerUpdateHandler)
+	router.DELETE("/api/customer/:id", customerDeleteHandler)
 	/*http.HandleFunc("/view/",  makeHandler(viewHandler))
 	  http.HandleFunc("/edit/",  makeHandler(editHandler))
 	  http.HandleFunc("/save/",  makeHandler(saveHandler))*/
 	http.ListenAndServe(":8070", router)
+	//fmt.Println(customer.GetCustomer(1))
 }
